25cf9154fd3127d5c7ae64318ffa6340f86ac9b5
Tomas Kotula <tomasrkotula@gmail.com>
2014-04-28 04:14:54 +0200

    updated desktop icon and title

--- desktop/src/cz/kotu/ld29/desktop/DesktopLauncher.java
+++ desktop/src/cz/kotu/ld29/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package cz.kotu.ld29.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import cz.kotu.ld29.MyGame;
@@ -7,6 +8,8 @@ import cz.kotu.ld29.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Ant Slaves :: by Tomas Kotula (Kotucz) :: for Ludum Dare 29";
+        config.addIcon("icon32.png", Files.FileType.Internal);
 		new LwjglApplication(new MyGame(), config);
 	}
 }
